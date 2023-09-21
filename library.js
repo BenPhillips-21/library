a7931986ddf7515bbb9c8257e7aa4f8965bfaf44
HolidayGuy21 <14449@coderacademy.edu.au>
2023-09-21 17:03:18 +1000

    added book adding functionality with popup form

---
+++ library.js
@@ -0,0 +1,61 @@
+const myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read ? "read" : "not read yet"
+    this.info = function() {
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const showFormButton = document.getElementById("showFormButton");
+    const libraryFormContainer = document.getElementById("libraryFormContainer");
+    const closeFormButton = document.getElementById("closeFormButton");
+
+    // Show the form when the button is clicked
+    showFormButton.addEventListener("click", function() {
+        libraryFormContainer.classList.remove("hidden");
+    });
+
+    // Hide the form when the "Close Form" button is clicked
+    closeFormButton.addEventListener("click", function() {
+        libraryFormContainer.classList.add("hidden");
+    });
+
+    // Optionally, prevent the form from submitting and add your form submission logic here
+    const libraryForm = document.getElementById("libraryForm");
+
+    libraryForm.addEventListener("submit", function(event) 
+    {
+        event.preventDefault();
+         // Get user input
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
+    const pagesInput = document.getElementById("pages");
+    const readInput = document.getElementById("read");
+
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = readInput.value;
+
+    // Create a new object using the constructor
+    const newBook = new Book(title, author, pages, read);
+
+    // Display the object in a list
+    const libraryList = document.getElementById("libraryList");
+    const objectItem = document.createElement("div");
+    objectItem.textContent = `title: ${newBook.title}, author: ${newBook.author}, pages: ${newBook.pages}, read: ${newBook.read}`;
+    libraryList.appendChild(objectItem);
+
+    // Clear the form inputs
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.value = "";
+    });
+});
+
